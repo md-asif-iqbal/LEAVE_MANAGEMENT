f60869cd78be341de3ef0d47ed077ef4fb4d4070
md-asif-iqbal <miqbal191034@bscse.uiu.ac.bd>
2023-03-07 15:25:44 +0600

    dashboard done

--- leave_entry.php
+++ leave_entry.php
@@ -7,8 +7,8 @@
     $query = "INSERT INTO APPLY_FOR_LEAVE(EMPLOYEE_NAME, LEAVE_START, LEAVE_END, STATUS)VALUES('$name', '$startDate', '$endDate', 'PENDING')";
     $create_query = mysqli_query($conn, $query);
     if($create_query){
-    echo 'Successfull';
+    echo 'Successfull......';
   }else{
-    echo "Unsuccessfull";
+    echo "Unsuccessfull...";
   }
 ?>
