e456efab05c935ae71de83885140930d63d54490
md-asif-iqbal <miqbal191034@bscse.uiu.ac.bd>
2023-03-07 14:50:38 +0600

    database and db connection done

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'LEAVE_MANAGEMENT';
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if($conn){
+echo 'successful';
+}else{
+echo 'failed';
+}
+?>
